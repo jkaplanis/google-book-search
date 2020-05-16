c3282fcad51357a6f983250581cb107a24513569
jkaplanis <johntkaplanis@gmail.com>
2020-05-15 21:19:08 -0700

    update axios call to take a query param from client and return the item data

--- controllers/google.js
+++ controllers/google.js
@@ -1,8 +1,40 @@
 const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 
 router.get("/googlebooks", (req, res) => {
-  res.send("google books is alive!");
+  if (!req.query.q) {
+    return res.status(400).send("Msust include 'q' param");
+  }
+  // example query : q=intitle:harry+potter
+
+  axios
+    .get("https://www.googleapis.com/books/v1/volumes", {
+      params: { q: req.query }
+    })
+    .then(response => {
+      const validResults = response.data.items.map(item => {
+        const googleId = item.id;
+        const {
+          title,
+          authors,
+          description,
+          imageLinks,
+          infoLink
+        } = item.volumeInfo;
+        const image = imageLinks.thumbnail;
+        const link = infoLink;
+      });
+      return res.json(response.data.items);
+    })
+    .catch(error => {
+      if (error.response) {
+        console.log(error.response.statusText);
+        res.sendStatus(error.response.status);
+      } else {
+        res.sendStatus(500);
+      }
+    });
 });
 
 module.exports = router;
