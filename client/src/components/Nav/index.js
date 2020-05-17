8c1d02a0c7e326b4a0e6a9b2a8c6cac0d2a91030
jkaplanis <johntkaplanis@gmail.com>
2020-05-16 19:22:16 -0700

    added href routes for nav bar link

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand" href="/">
         Google Books
       </a>
       <button
@@ -20,12 +20,12 @@ function Navbar() {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="/">
               Search <span className="sr-only">(current)</span>
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="/books">
               Saved
             </a>
           </li>
